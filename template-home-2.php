39c58fbe3448f382f6d528153cb0e17f28c359ea
Kevin McMahon <kevinmcmahon@obelismedia.com>
2015-05-11 17:50:04 -0400

    [deploy: production]
    More Home Page Updates

--- template-home-2.php
+++ template-home-2.php
@@ -51,7 +51,7 @@
   </div><!-- /.row -->
 </div> <!--/.container -->
 
-<div class="container-fluid">
+<div class="container-fluid jumbotron">
 <div style="width:100%; height:40px; background-color:#4d8bc2;">
 </div>
 </div>
@@ -60,23 +60,32 @@
 	<div class="row">
     	<div class="col-md-4">
         	<h2>Practice Information</h2>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras aliquam dolor sit amet erat tempor rhoncus. Curabitur tellus arcu, interdum auctor mi non, imperdiet sagittis felis. Duis imperdiet vel nunc eget elementum. Integer in ultrices est, non adipiscing ligula. Fusce commodo, magna vel consequat pretium, urna magna egestas risus, ut.</p>
-            <a class="btn btn-large btn-primary" href="#">Read More</a>
+            <p>The Law Offices of Eric Richman provides outstanding personal injury legal representation to individuals and families who suffered injuries in and around New York, including New York City, Bronx, Queens, Manhattan, Brooklyn, Staten Island, Rockland, Westchester, Long Island, Nassau County or Suffolk County.</p>
+            <a class="btn btn-large btn-primary" href="#">Read More <span class="glyphicon glyphicon-arrow-right"></span></a>
         </div>
         <div class="col-md-4">
         	<h2>About Eric Richman</h2>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras aliquam dolor sit amet erat tempor rhoncus. Curabitur tellus arcu, interdum auctor mi non, imperdiet sagittis felis. Duis imperdiet vel nunc eget elementum. Integer in ultrices est, non adipiscing ligula. Fusce commodo, magna vel consequat pretium, urna magna egestas risus, ut.</p>
-            <a class="btn btn-large btn-primary" href="#">Sign up today</a>
+            <p>Eric Richman, Esq. is a passionate and experienced New York City personal injury lawyer who has dedicated his career to fighting for injury victims and their families.  Mr. Richman has won millions of dollars for his clients and is one of the best known and most respected personal injury lawyers in NYC </p>
+            <a class="btn btn-large btn-primary" href="#">Read More <span class="glyphicon glyphicon-arrow-right"></span></a>
         </div>
         <div class="col-md-4">
         	<h2>Practice Areas</h2>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras aliquam dolor sit amet erat tempor rhoncus. Curabitur tellus arcu, interdum auctor mi non, imperdiet sagittis felis. Duis imperdiet vel nunc eget elementum. Integer in ultrices est, non adipiscing ligula. Fusce commodo, magna vel consequat pretium, urna magna egestas risus, ut.</p>
-            <a class="btn btn-large btn-primary" href="#">Sign up today</a>
+            <ul>
+            	<li>Personal Injury</li>
+                <li>Car and Truck Accidents</li>
+                <li>Bus and Subway Accidents</li>
+                <li>Motorcycle Bicycle Accidents</li>
+                <li>Premises Liability</li>
+                <li>Sexual Assualt Victims</li>
+                <li>Medical Malpractice</li>
+                <li>Wrongful Death and Catastrophic Injuries</li>
+            </ul>
+            <a class="btn btn-large btn-primary" href="#">Read More <span class="glyphicon glyphicon-arrow-right"></span></a>
         </div>
 	</div><!--/.row-->
 </div><!-- /.container -->
 
-<div class="container-fluid">
+<div class="container-fluid jumbotron">
 <div style="width:100%; height:40px; background-color:#4d8bc2;">
 </div>
 </div>
@@ -89,14 +98,22 @@
     </div><!--  /.row -->
 	<div class="row">
     	<div class="col-md-4">
+        	<div class="well">
         	<h3><span class="glyphicon glyphicon-star"></span><span class="glyphicon glyphicon-star"></span><span class="glyphicon glyphicon-star"></span><span class="glyphicon glyphicon-star"></span><span class="glyphicon glyphicon-star"></span><br /><small>Kelsey Plantas</small></h3><p>Eric is a great attorney. He is highly knowledgable, professional, and personable. He made me feel comfortable throughout the whole process of settling my case. Eric also made sure that I understood every step towards settlement, which made me feel more at ease with the decisions I had to make. I would definitely recommend working with Eric.</p>
+            </div>
         </div>
 		<div class="col-md-4">
+        	<div class="well">
         	<h3><span class="glyphicon glyphicon-star"></span><span class="glyphicon glyphicon-star"></span><span class="glyphicon glyphicon-star"></span><span class="glyphicon glyphicon-star"></span><span class="glyphicon glyphicon-star"></span><br /><small>Marisa Kronenberg</small></h3><p>Eric Richman is very honest, intelligent, and easy to talk to. He made himself available to me whenever I had questions and I always felt at ease knowing he was working hard for my case. He exceeded my expectations and I would recommend him to anyone looking for a personal injury lawyer!</p>
+            </div>
         </div>
         <div class="col-md-4">
+        	<div class="well">
         	<h3><span class="glyphicon glyphicon-star"></span><span class="glyphicon glyphicon-star"></span><span class="glyphicon glyphicon-star"></span><span class="glyphicon glyphicon-star"></span><span class="glyphicon glyphicon-star"></span><br /><small>Paula Medeiros</small></h3><p>Extremely aggressive. Have already recommended him to a friend who also got in an accident. 10 out of 10!</p>
+            </div>
+            <div class="well">
             <h3><span class="glyphicon glyphicon-star"></span><span class="glyphicon glyphicon-star"></span><span class="glyphicon glyphicon-star"></span><span class="glyphicon glyphicon-star"></span><span class="glyphicon glyphicon-star"></span><br /><small>Shad Azimi</small></h3><p>Excellent communicator with deep knowledge of the legal process. Also very responsive. Highly recommended.</p>
+            </div>
         </div>
     </div><!--/row-->
     <div class="row">
