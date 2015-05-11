26745288968af2d5a601b7d87e0b70bee555afd4
Kevin McMahon <kevinmcmahon@obelismedia.com>
2015-05-11 17:01:10 -0400

    Update Home Template as a Copy
    Made layout and content changes to the home page and saved it as
    “template-home-2” to produce a copy.

---
+++ template-home-2.php
@@ -0,0 +1,104 @@
+<?php
+/*
+ * Template Name: Home Page
+ * Description: Page without sidebar
+ */
+ ?>
+ <?php get_header(); ?>
+
+
+<?php /* include(TEMPLATEPATH . '/inc/carousel-container.php'); */ ?>
+
+<?php // include(TEMPLATEPATH . '/inc/carousel-full-width.php'); ?>
+</div>
+<div id="myCarousel" class="carousel slide visible-lg visible-md">
+<div class="carousel-inner">
+        <div class="item active" style="background-image: url(/wp-content/uploads/sites/12/2015/04/Queensboro-Bridge-over-New-Yor-web.jpg);background-position:center; background-size:cover;background-repeat: no-repeat;">
+<!--           <img src="http://lorempixel.com/g/1500/600/city/1" alt="First slide"> -->
+          <div class="container">
+            <div class="carousel-caption">
+              <h1>Example headline.</h1>
+              <p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
+              <p><a class="btn btn-large btn-primary" href="#">Sign up today</a></p>
+            </div>
+          </div>
+        </div>
+
+</div>
+</div>        
+
+<div class="container">
+  <!-- Three columns of text below the carousel -->
+  <div class="row">
+    <div class="col-md-4 text-center">
+      <img class="img-circle" src="http://placehold.it/140x140">
+      <h2>Mobile-first</h2>
+      <p>Tablets, phones, laptops. The new 3 promises to be mobile friendly from the start.</p>
+      <p><a class="btn btn-primary" href="#">View details <span class="glyphicon glyphicon-arrow-right"></span></a></p>
+    </div>
+    <div class="col-md-4 text-center">
+      <img class="img-circle" src="http://placehold.it/140x140">
+      <h2>One Fluid Grid</h2>
+      <p>There is now just one percentage-based grid for Bootstrap 3. Customize for fixed widths.</p>
+      <p><a class="btn btn-primary" href="#">View details <span class="glyphicon glyphicon-arrow-right"></span></a></p>
+    </div>
+    <div class="col-md-4 text-center">
+      <img class="img-circle" src="http://placehold.it/140x140">
+      <h2>LESS is More</h2>
+      <p>Improved support for mixins make the new Bootstrap 3 easier to customize.</p>
+      <p><a class="btn btn-primary" href="#">View details <span class="glyphicon glyphicon-arrow-right"></span></a></p>
+    </div>
+  </div><!-- /.row -->
+</div> <!--/.container -->
+
+<div class="container-fluid">
+</div>
+
+<div class="container">
+	<div class="row">
+    	<div class="col-md-4">
+        	<h2>Practice Information</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras aliquam dolor sit amet erat tempor rhoncus. Curabitur tellus arcu, interdum auctor mi non, imperdiet sagittis felis. Duis imperdiet vel nunc eget elementum. Integer in ultrices est, non adipiscing ligula. Fusce commodo, magna vel consequat pretium, urna magna egestas risus, ut.</p>
+            <a class="btn btn-large btn-primary" href="#">Read More</a>
+        </div>
+        <div class="col-md-4">
+        	<h2>About Eric Richman</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras aliquam dolor sit amet erat tempor rhoncus. Curabitur tellus arcu, interdum auctor mi non, imperdiet sagittis felis. Duis imperdiet vel nunc eget elementum. Integer in ultrices est, non adipiscing ligula. Fusce commodo, magna vel consequat pretium, urna magna egestas risus, ut.</p>
+            <a class="btn btn-large btn-primary" href="#">Sign up today</a>
+        </div>
+        <div class="col-md-4">
+        	<h2>Practice Areas</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras aliquam dolor sit amet erat tempor rhoncus. Curabitur tellus arcu, interdum auctor mi non, imperdiet sagittis felis. Duis imperdiet vel nunc eget elementum. Integer in ultrices est, non adipiscing ligula. Fusce commodo, magna vel consequat pretium, urna magna egestas risus, ut.</p>
+            <a class="btn btn-large btn-primary" href="#">Sign up today</a>
+        </div>
+	</div><!--/.row-->
+</div><!-- /.container -->
+
+<div class="container-fluid">
+</div>
+
+<div class="container reviews">
+	<div class="row">
+    	<div class="col-xs-12">
+        	<h2>Reviews</h2>
+        </div>
+    </div><!--  /.row -->
+	<div class="row">
+    	<div class="col-md-4">
+        	<h3><span class="glyphicon glyphicon-star"></span><span class="glyphicon glyphicon-star"></span><span class="glyphicon glyphicon-star"></span><span class="glyphicon glyphicon-star"></span><span class="glyphicon glyphicon-star"></span><br /><small>Kelsey Plantas</small></h3><p>Eric is a great attorney. He is highly knowledgable, professional, and personable. He made me feel comfortable throughout the whole process of settling my case. Eric also made sure that I understood every step towards settlement, which made me feel more at ease with the decisions I had to make. I would definitely recommend working with Eric.</p>
+        </div>
+		<div class="col-md-4">
+        	<h3><span class="glyphicon glyphicon-warning-sign"></span><span class="glyphicon glyphicon-warning-sign"></span><span class="glyphicon glyphicon-warning-sign"></span><span class="glyphicon glyphicon-warning-sign"></span><span class="glyphicon glyphicon-warning-sign"></span><br /><small>Marisa Kronenberg</small></h3><p>Eric Richman is very honest, intelligent, and easy to talk to. He made himself available to me whenever I had questions and I always felt at ease knowing he was working hard for my case. He exceeded my expectations and I would recommend him to anyone looking for a personal injury lawyer!</p>
+        </div>
+        <div class="col-md-4">
+        	<h3><span class="glyphicon glyphicon-cloud"></span><span class="glyphicon glyphicon-cloud"></span><span class="glyphicon glyphicon-cloud"></span><span class="glyphicon glyphicon-cloud"></span><span class="glyphicon glyphicon-cloud"></span><br /><small>Paula Medeiros</small></h3><p>Extremely aggressive. Have already recommended him to a friend who also got in an accident. 10 out of 10!</p>
+            <h3><span class="glyphicon glyphicon-cloud"></span><span class="glyphicon glyphicon-cloud"></span><span class="glyphicon glyphicon-cloud"></span><span class="glyphicon glyphicon-cloud"></span><span class="glyphicon glyphicon-cloud"></span><br /><small>Shad Azimi</small></h3><p>Excellent communicator with deep knowledge of the legal process. Also very responsive. Highly recommended.</p>
+        </div>
+    </div><!--/row-->
+    <div class="row">
+    	<div class="col-xs-12">
+    		<a class="btn btn-large btn-primary" href="https://plus.google.com/104401465280386732812/" target="_blank">Read More Reviews</a>
+        </div>
+    </div><!-- /.row -->
+</div><!-- /.container -->
+<?php get_footer(); ?>
