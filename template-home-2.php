361c5e3726c78ba94dcb1f5e215c26e7db221f5b
Kevin McMahon <kevinmcmahon@obelismedia.com>
2015-05-12 13:49:21 -0400

    Some More Home Page Updates
    Various container changes and added new headline/tagline

--- template-home-2.php
+++ template-home-2.php
@@ -12,20 +12,17 @@
 <?php // include(TEMPLATEPATH . '/inc/carousel-full-width.php'); ?>
 </div>
 <div id="myCarousel" class="carousel slide visible-lg visible-md">
-<div class="carousel-inner">
+	<div class="carousel-inner">
         <div class="item active" style="background-image: url(/wp-content/uploads/sites/12/2015/04/Queensboro-Bridge-over-New-Yor-web.jpg);background-position:center; background-size:cover;background-repeat: no-repeat;">
-<!--           <img src="http://lorempixel.com/g/1500/600/city/1" alt="First slide"> -->
-          <div class="container">
-            <div class="carousel-caption">
-              <h1>Example headline.</h1>
-              <p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
-              <p><a class="btn btn-large btn-primary" href="#">Sign up today</a></p>
-            </div>
-          </div>
-        </div>
-
-</div>
-</div>        
+        	<div class="container">
+            	<div class="carousel-caption">
+                	<h1>Personal Injury Justice for New York City</h1>
+                    <p>The Law Offices of Eric Richman specializes on Personal Injury law in New York City. Our team of experienced New York trial attorneys work with leading investigators, medial experts and financial experts to prepare a strong case. Our success is contingent on yours.</p>
+                </div><!-- /.carousel-caption -->
+            </div><!-- /.container -->
+        </div><!-- /.item -->
+	</div><!-- /.carousel-inner -->
+</div><!-- carousel -->
 
 <div class="container">
   <!-- Three columns of text below the carousel -->
@@ -51,75 +48,83 @@
   </div><!-- /.row -->
 </div> <!--/.container -->
 
-<div class="container-fluid jumbotron">
-<div style="width:100%; height:40px; background-color:#4d8bc2;">
+<div class="container-fluid">
+<div style="width:100%; height:100px; background-color:#4d8bc2; margin: 20px auto;">
 </div>
 </div>
 
 <div class="container">
-	<div class="row">
+	<div class="row height-fix">
     	<div class="col-md-4">
-        	<h2>Practice Information</h2>
-            <p>The Law Offices of Eric Richman provides outstanding personal injury legal representation to individuals and families who suffered injuries in and around New York, including New York City, Bronx, Queens, Manhattan, Brooklyn, Staten Island, Rockland, Westchester, Long Island, Nassau County or Suffolk County.</p>
-            <a class="btn btn-large btn-primary" href="#">Read More <span class="glyphicon glyphicon-arrow-right"></span></a>
+        	<div class="well height-fix-item">
+                <h2>Practice Information</h2>
+                <p>The Law Offices of Eric Richman provides outstanding personal injury legal representation to individuals and families who suffered injuries in and around New York, including New York City, Bronx, Queens, Manhattan, Brooklyn, Staten Island, Rockland, Westchester, Long Island, Nassau County or Suffolk County.</p>
+                <a class="btn btn-large btn-primary" href="#">Read More <span class="glyphicon glyphicon-arrow-right"></span></a>
+            </div>
         </div>
         <div class="col-md-4">
-        	<h2>About Eric Richman</h2>
-            <p>Eric Richman, Esq. is a passionate and experienced New York City personal injury lawyer who has dedicated his career to fighting for injury victims and their families.  Mr. Richman has won millions of dollars for his clients and is one of the best known and most respected personal injury lawyers in NYC </p>
-            <a class="btn btn-large btn-primary" href="#">Read More <span class="glyphicon glyphicon-arrow-right"></span></a>
+        	<div class="well height-fix-item">
+                <h2>About Eric Richman</h2>
+                <p>Eric Richman, Esq. is a passionate and experienced New York City personal injury lawyer who has dedicated his career to fighting for injury victims and their families.  Mr. Richman has won millions of dollars for his clients and is one of the best known and most respected personal injury lawyers in NYC </p>
+                <a class="btn btn-large btn-primary" href="#">Read More <span class="glyphicon glyphicon-arrow-right"></span></a>
+            </div>
         </div>
         <div class="col-md-4">
-        	<h2>Practice Areas</h2>
-            <ul>
-            	<li>Personal Injury</li>
-                <li>Car and Truck Accidents</li>
-                <li>Bus and Subway Accidents</li>
-                <li>Motorcycle Bicycle Accidents</li>
-                <li>Premises Liability</li>
-                <li>Sexual Assualt Victims</li>
-                <li>Medical Malpractice</li>
-                <li>Wrongful Death and Catastrophic Injuries</li>
-            </ul>
-            <a class="btn btn-large btn-primary" href="#">Read More <span class="glyphicon glyphicon-arrow-right"></span></a>
+        	<div class="well height-fix-item">
+                <h2>Practice Areas</h2>
+                <ul>
+                    <li>Personal Injury</li>
+                    <li>Car and Truck Accidents</li>
+                    <li>Bus and Subway Accidents</li>
+                    <li>Motorcycle Bicycle Accidents</li>
+                    <li>Premises Liability</li>
+                    <li>Sexual Assualt Victims</li>
+                    <li>Medical Malpractice</li>
+                    <li>Wrongful Death and Catastrophic Injuries</li>
+                </ul>
+                <a class="btn btn-large btn-primary" href="#">Read More <span class="glyphicon glyphicon-arrow-right"></span></a>
+            </div>
         </div>
 	</div><!--/.row-->
 </div><!-- /.container -->
 
-<div class="container-fluid jumbotron">
-<div style="width:100%; height:40px; background-color:#4d8bc2;">
+<div class="container-fluid">
+<div style="width:100%; height:100px; background-color:#4d8bc2; margin: 20px auto;">
 </div>
 </div>
 
 <div class="container reviews">
-	<div class="row">
-    	<div class="col-xs-12">
-        	<h2>Reviews</h2>
-        </div>
-    </div><!--  /.row -->
-	<div class="row">
-    	<div class="col-md-4">
-        	<div class="well">
-        	<h3><span class="glyphicon glyphicon-star"></span><span class="glyphicon glyphicon-star"></span><span class="glyphicon glyphicon-star"></span><span class="glyphicon glyphicon-star"></span><span class="glyphicon glyphicon-star"></span><br /><small>Kelsey Plantas</small></h3><p>Eric is a great attorney. He is highly knowledgable, professional, and personable. He made me feel comfortable throughout the whole process of settling my case. Eric also made sure that I understood every step towards settlement, which made me feel more at ease with the decisions I had to make. I would definitely recommend working with Eric.</p>
+	<div class="jumbotron">
+        <div class="row">
+            <div class="col-xs-12">
+                <h2>Reviews</h2>
             </div>
-        </div>
-		<div class="col-md-4">
-        	<div class="well">
-        	<h3><span class="glyphicon glyphicon-star"></span><span class="glyphicon glyphicon-star"></span><span class="glyphicon glyphicon-star"></span><span class="glyphicon glyphicon-star"></span><span class="glyphicon glyphicon-star"></span><br /><small>Marisa Kronenberg</small></h3><p>Eric Richman is very honest, intelligent, and easy to talk to. He made himself available to me whenever I had questions and I always felt at ease knowing he was working hard for my case. He exceeded my expectations and I would recommend him to anyone looking for a personal injury lawyer!</p>
+        </div><!--  /.row -->
+        <div class="row">
+            <div class="col-md-4">
+                <div class="well">
+                <h3><span class="glyphicon glyphicon-star"></span><span class="glyphicon glyphicon-star"></span><span class="glyphicon glyphicon-star"></span><span class="glyphicon glyphicon-star"></span><span class="glyphicon glyphicon-star"></span><br /><small>Kelsey Plantas</small></h3><p>Eric is a great attorney. He is highly knowledgable, professional, and personable. He made me feel comfortable throughout the whole process of settling my case. Eric also made sure that I understood every step towards settlement, which made me feel more at ease with the decisions I had to make. I would definitely recommend working with Eric.</p>
+                </div>
             </div>
-        </div>
-        <div class="col-md-4">
-        	<div class="well">
-        	<h3><span class="glyphicon glyphicon-star"></span><span class="glyphicon glyphicon-star"></span><span class="glyphicon glyphicon-star"></span><span class="glyphicon glyphicon-star"></span><span class="glyphicon glyphicon-star"></span><br /><small>Paula Medeiros</small></h3><p>Extremely aggressive. Have already recommended him to a friend who also got in an accident. 10 out of 10!</p>
+            <div class="col-md-4">
+                <div class="well">
+                <h3><span class="glyphicon glyphicon-star"></span><span class="glyphicon glyphicon-star"></span><span class="glyphicon glyphicon-star"></span><span class="glyphicon glyphicon-star"></span><span class="glyphicon glyphicon-star"></span><br /><small>Marisa Kronenberg</small></h3><p>Eric Richman is very honest, intelligent, and easy to talk to. He made himself available to me whenever I had questions and I always felt at ease knowing he was working hard for my case. He exceeded my expectations and I would recommend him to anyone looking for a personal injury lawyer!</p>
+                </div>
             </div>
-            <div class="well">
-            <h3><span class="glyphicon glyphicon-star"></span><span class="glyphicon glyphicon-star"></span><span class="glyphicon glyphicon-star"></span><span class="glyphicon glyphicon-star"></span><span class="glyphicon glyphicon-star"></span><br /><small>Shad Azimi</small></h3><p>Excellent communicator with deep knowledge of the legal process. Also very responsive. Highly recommended.</p>
+            <div class="col-md-4">
+                <div class="well">
+                <h3><span class="glyphicon glyphicon-star"></span><span class="glyphicon glyphicon-star"></span><span class="glyphicon glyphicon-star"></span><span class="glyphicon glyphicon-star"></span><span class="glyphicon glyphicon-star"></span><br /><small>Paula Medeiros</small></h3><p>Extremely aggressive. Have already recommended him to a friend who also got in an accident. 10 out of 10!</p>
+                </div>
+                <div class="well">
+                <h3><span class="glyphicon glyphicon-star"></span><span class="glyphicon glyphicon-star"></span><span class="glyphicon glyphicon-star"></span><span class="glyphicon glyphicon-star"></span><span class="glyphicon glyphicon-star"></span><br /><small>Shad Azimi</small></h3><p>Excellent communicator with deep knowledge of the legal process. Also very responsive. Highly recommended.</p>
+                </div>
             </div>
-        </div>
-    </div><!--/row-->
-    <div class="row">
-    	<div class="col-xs-12">
-    		<a class="btn btn-large btn-primary" href="https://plus.google.com/104401465280386732812/" target="_blank">Read More Reviews</a>
-        </div>
-    </div><!-- /.row -->
+        </div><!--/row-->
+        <div class="row">
+            <div class="col-xs-12">
+                <a class="btn btn-large btn-primary" href="https://plus.google.com/104401465280386732812/" target="_blank">Read More Reviews</a>
+            </div>
+    	</div><!-- /.row -->
+	</div><!-- /.jumbotron -->
 </div><!-- /.container -->
 <?php get_footer(); ?>
