a7f4925bee3da77c669f9593383c615409befb96
Kevin McMahon <kevinmcmahon@obelismedia.com>
2015-05-12 17:25:07 -0400

    Vertical Center Again 2

--- header.php
+++ header.php
@@ -22,8 +22,8 @@
 	
 		<img src="data:image/png;base64," data-src="<?php bloginfo('template_url'); ?>/js/holder.js/400x100/text:LOGO" alt="First slide" class="img-responsive img-thumbnail logo" />
         	</div>
-            <div class="col-lg-8 col-md-8 col-sm-9">
-                <div class="navbar-wrapper vertical-center">
+            <div class="col-lg-8 col-md-8 col-sm-9 vertical-center">
+                <div class="navbar-wrapper">
                     <div class="navbar navbar-default">
                         <div class="navbar-header">
                             <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
