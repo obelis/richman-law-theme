3fd7b2b6545be6063dbde96d2685f9d354e32f8e
Kevin McMahon <kevinmcmahon@obelismedia.com>
2015-05-12 16:10:15 -0400

    Height Fix Fingers crossed

--- js/height-fix.js
+++ js/height-fix.js
@@ -26,8 +26,8 @@ function heightFix(){
         // Get First Key in maxHeight Array and convert to string for CSS
         for (ib = 0; ib < heightFixItems.length; ib++){
             heightFixItems[ib].style.height = itemHeights[0] + "px";
-			jQuery(heightFixItems[ib] > '.height-fix-bottom').css("position", "absolute");
-			jQuery(heightFixItems[ib] > '.height-fix-bottom').css("bottom", "0px");
+			jQuery('.height-fix-bottom',heightFixItems[ib]).css("position", "absolute");
+			jQuery('.height-fix-bottom',heightFixItems[ib]).css("bottom", "0px");
         }
     }
 }
