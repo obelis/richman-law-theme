c27aa1dd1baa4413b05395a0877aad332eb058a0
Kevin McMahon <kevinmcmahon@obelismedia.com>
2015-05-12 17:45:29 -0400

    Moved Vertical Center Class

--- header.php
+++ header.php
@@ -17,13 +17,13 @@
 </head>
 <body <?php body_class(); ?>>
 	<div class="container header">
-    	<div class="row">
+    	<div class="row vertical-center">
         	<div class="col-lg-4 col-md-4 col-sm-3">
 	
 		<img src="data:image/png;base64," data-src="<?php bloginfo('template_url'); ?>/js/holder.js/400x100/text:LOGO" alt="First slide" class="img-responsive img-thumbnail logo" />
         	</div>
             <div class="col-lg-8 col-md-8 col-sm-9">
-                <div class="navbar-wrapper vertical-center">
+                <div class="navbar-wrapper">
                     <div class="navbar navbar-default">
                         <div class="navbar-header">
                             <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
