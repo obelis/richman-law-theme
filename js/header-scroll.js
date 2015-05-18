70f34862c4e2497a39a57f9d79eef16fe86a6cfc
Kevin McMahon <kevinmcmahon@obelismedia.com>
2015-05-18 13:48:49 -0400

    Header Scroll Effect

--- js/header-scroll.js
+++ js/header-scroll.js
@@ -1,6 +1,7 @@
 jQuery(window).on('scroll', function(){
-	console.log('It is running');
-	if (jQuery(this).offset() > 20){
+	console.log('It is running again');
+	var scrollPosition = jQuery(this).offset();
+	if (scrollPosition.top > 20){
 		jQuery('.header').addClass('scrolled');
 	} else {
 		if (jQuery('.header').has('scrolled')){
