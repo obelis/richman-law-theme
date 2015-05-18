d0256aacab4e5577d7e3bc75d587139a49ae32d3
Kevin McMahon <kevinmcmahon@obelismedia.com>
2015-05-18 14:09:51 -0400

    Header Scroll Effect

--- js/header-scroll.js
+++ js/header-scroll.js
@@ -1,11 +1,18 @@
 jQuery(window).on('scroll', function(){
-	console.log('RUN');
 	var scrollPosition = jQuery('.header').offset();
 	if (scrollPosition.top > 20){
 		jQuery('.header').addClass('scrolled');
+		if (jQuery('.header').has('container')){
+			jQuery('.header').addClass('container-fluid');
+			jQuery('.header').removeClass('container');
+		}
 	} else {
 		if (jQuery('.header').has('scrolled')){
 			jQuery('.header').removeClass('scrolled');
+		}
+		if (jQuery('.header').has('container-fluid')){
+			jQuery('.header').addClass('container');
+			jQuery('.header').removeClass('container-fluid');
 		}
 	}
 });
