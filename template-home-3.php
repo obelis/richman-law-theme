0885afadc48f46c530a5b9ea0cf432c3f34b7c96
Kevin McMahon <kevinmcmahon@obelismedia.com>
2015-05-20 18:20:45 -0400

    Home Page Link Updates

--- template-home-3.php
+++ template-home-3.php
@@ -65,14 +65,14 @@
         	<div class="well info-box height-fix-item vertical-top horizontal-center">
                 <h2>Practice Information</h2>
                 <p>The Law Offices of Eric Richman provides outstanding personal injury legal representation to individuals and families who suffered injuries in and around New York, including New York City, Bronx, Queens, Manhattan, Brooklyn, Staten Island, Rockland, Westchester, Long Island, Nassau County or Suffolk County.</p>
-                <a class="btn btn-large btn-primary vertical-push-bottom" href="#">Read More <span class="glyphicon glyphicon-arrow-right"></span></a>
+                <a class="btn btn-large btn-primary vertical-push-bottom" href="/about-us/">Read More <span class="glyphicon glyphicon-arrow-right"></span></a>
             </div>
         </div>
         <div class="col-md-4">
         	<div class="well info-box height-fix-item vertical-top horizontal-center">
                 <h2>About Eric Richman</h2>
                 <p>Eric Richman, Esq. is a passionate and experienced New York City personal injury lawyer who has dedicated his career to fighting for injury victims and their families.  Mr. Richman has won millions of dollars for his clients and is one of the best known and most respected personal injury lawyers in NYC </p>
-                <a class="btn btn-large btn-primary vertical-push-bottom" href="#">Read More <span class="glyphicon glyphicon-arrow-right"></span></a>
+                <a class="btn btn-large btn-primary vertical-push-bottom" href="/attorneys/">Read More <span class="glyphicon glyphicon-arrow-right"></span></a>
             </div>
         </div>
         <div class="col-md-4">
@@ -88,7 +88,7 @@
                     <li>Medical Malpractice</li>
                     <li>Wrongful Death and Catastrophic Injuries</li>
                 </ul>
-                <a class="btn btn-large btn-primary vertical-push-bottom" href="#">Read More <span class="glyphicon glyphicon-arrow-right"></span></a>
+                <a class="btn btn-large btn-primary vertical-push-bottom" href="/practice-areas/">Read More <span class="glyphicon glyphicon-arrow-right"></span></a>
             </div>
         </div>
 	</div><!--/.row-->
