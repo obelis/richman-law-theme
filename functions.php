d39355e7f8a84f806a30823f70d627db12463752
Mike Harrigan <michaelharrigan@obelismedia.com>
2015-05-20 14:56:43 -0400

    child pages

--- functions.php
+++ functions.php
@@ -592,9 +592,9 @@ global $post;
 
 if ( is_page() && $post->post_parent ) 
 
-	$childpages = wp_list_pages( 'sort_column=menu_order&title_li=&depth=1&child_of=' . $post->post_parent . '&echo=0' );
+	$childpages = 'parent'.wp_list_pages( 'sort_column=menu_order&title_li=&depth=1&child_of=' . $post->post_parent . '&echo=0' );
 else
-	$childpages = wp_list_pages( 'sort_column=menu_order&title_li=&depth=1&child_of=' . $post->ID . '&echo=0' );
+	$childpages = 'ID'.wp_list_pages( 'sort_column=menu_order&title_li=&depth=1&child_of=' . $post->ID . '&echo=0' );
 
 if ( $childpages ) {
 
