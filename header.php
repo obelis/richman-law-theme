4a54c5af33bc18e3088885e6e82bb5639d503242
Mike Harrigan <michaelharrigan@obelismedia.com>
2015-05-20 15:02:39 -0400

    remove meta from header

--- header.php
+++ header.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8">
     <title><?php bloginfo('description'); ?></title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="description" content="">
+<!--     <meta name="description" content=""> -->
     <meta name="author" content="">
 
 	<link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>" type="text/css" />
