f053c8443e45b8f0c8997b5602cd7dd78d0abd5a
Mike Harrigan <michaelharrigan@obelismedia.com>
2015-05-20 12:19:05 -0400

    location vars

---
+++ inc/location_variables.php
@@ -0,0 +1,107 @@
+<?php
+$display_slogan = of_get_option('display_slogan');	
+$company_name = of_get_option('company_name');	
+$slogan = of_get_option('slogan');
+$display_full_address = of_get_option('display_full_address');
+///
+$display_2_location = of_get_option('show_second_office');
+$display_3_location = of_get_option('show_third_office');
+$display_4_location = of_get_option('show_fourth_office');
+$display_5_location = of_get_option('show_fifth_office');
+$display_6_location = of_get_option('show_sixth_office');
+$display_7_location = of_get_option('show_seventh_office');
+///
+$main_phone_number = of_get_option('main_phone_number');
+$main_secondary_phone_number = of_get_option('main_secondary_phone_number');
+$main_street = of_get_option('street_address');
+$main_city = of_get_option('main_city');
+$main_state = of_get_option('main_state');
+$main_zip = of_get_option('main_zip');
+$main_plaza = of_get_option('main_plaza');
+$street_address_second_line = of_get_option('street_address_second_line');
+$second_phone_number = of_get_option('second_phone_number');
+$second_street = of_get_option('second_street_address');
+$second_street_address_second_line = of_get_option('second_street_address_second_line');
+$second_city = of_get_option('second_city');
+$second_state = of_get_option('second_state');
+$second_zip = of_get_option('second_zip');
+$second_plaza = of_get_option('scond_plaza');
+$third_phone_number = of_get_option('third_phone_number');
+$third_street = of_get_option('third_street_address');
+$third_city = of_get_option('third_city');
+$third_state = of_get_option('third_state');
+$third_zip = of_get_option('third_zip');
+$third_plaza = of_get_option('third_plaza');
+$fourth_phone_number = of_get_option('fourth_phone_number');
+$fourth_street = of_get_option('fourth_street_address');
+$fourth_city = of_get_option('fourth_city');
+$fourth_state = of_get_option('fourth_state');
+$fourth_zip = of_get_option('fourth_zip');
+$fourth_plaza = of_get_option('fourth_plaza');
+$fifth_phone_number = of_get_option('fifth_phone_number');
+$fifth_street = of_get_option('fifth_street_address');
+$fifth_city = of_get_option('fifth_city');
+$fifth_state = of_get_option('fifth_state');
+$fifth_zip = of_get_option('fifth_zip');
+$fifth_plaza = of_get_option('fifth_plaza');
+$sixth_phone_number = of_get_option('sixth_phone_number');
+$sixth_street = of_get_option('sixth_street_address');
+$sixth_city = of_get_option('sixth_city');
+$sixth_state = of_get_option('sixth_state');
+$sixth_zip = of_get_option('sixth_zip');
+$sixth_plaza = of_get_option('sixth_plaza');
+$seventh_phone_number = of_get_option('seventh_phone_number');
+$seventh_street = of_get_option('seventh_street_address');
+$seventh_city = of_get_option('seventh_city');
+$seventh_state = of_get_option('seventh_state');
+$seventh_zip = of_get_option('seventh_zip');
+$seventh_plaza = of_get_option('seventh_plaza');
+/* bio variables*/
+$owner_name=of_get_option('owner_name');
+$owner_headshot=of_get_option('owner_headshot');
+$owner_title=of_get_option('owner_title');
+$owner_school=of_get_option('owner_school');
+$owner_school_year=of_get_option('owner_school_year');
+
+$owner_bio=of_get_option('owner_bio');
+	$owner_bio = wpautop($owner_bio);
+
+///
+$show_first_employee = of_get_option('show_first_employee');
+$employee_one_name=of_get_option('employee_one_name');
+$employee_one_headshot=of_get_option('employee_one_headshot');
+$employee_one_title=of_get_option('employee_one_title');
+$employee_one_school=of_get_option('employee_one_school');
+$employee_one_school_year=of_get_option('employee_one_school_year');
+$employee_one_bio=of_get_option('employee_one_bio');
+	$employee_one_bio = wpautop($employee_one_bio);
+
+///
+$show_second_employee = of_get_option('show_second_employee');
+$employee_two_name=of_get_option('employee_two_name');
+$employee_two_headshot=of_get_option('employee_two_headshot');
+$employee_two_title=of_get_option('employee_two_title');
+$employee_two_school=of_get_option('employee_two_school');
+$employee_two_school_year=of_get_option('employee_two_school_year');
+$employee_two_bio=of_get_option('employee_two_bio');
+	$employee_two_bio = wpautop($employee_two_bio);
+
+///
+$show_third_employee = of_get_option('show_third_employee');
+$employee_three_name=of_get_option('employee_three_name');
+$employee_three_headshot=of_get_option('employee_three_headshot');
+$employee_three_title=of_get_option('employee_three_title');
+$employee_three_school=of_get_option('employee_three_school');
+$employee_three_school_year=of_get_option('employee_three_school_year');
+$employee_three_bio=of_get_option('employee_three_bio');
+	$employee_three_bio = wpautop($employee_three_bio);
+///
+$employee_fourth_name=of_get_option('employee_fourth_name');
+$employee_fourth_headshot=of_get_option('employee_fourth_headshot');
+$employee_fourth_title=of_get_option('employee_fourth_title');
+$employee_fourth_school=of_get_option('employee_fourth_school');
+$employee_fourth_school_year=of_get_option('employee_fourth_school_year');
+$employee_fourth_bio=of_get_option('employee_fourth_bio');
+	$employee_fourth_bio = wpautop($employee_fourth_bio);
+
+?>
