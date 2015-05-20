dbcb1b39c1113f2cca170dd77a56d2c978cd1093
Mike Harrigan <michaelharrigan@obelismedia.com>
2015-05-20 11:57:57 -0400

    options

---
+++ options-areas.php
@@ -0,0 +1,171 @@
+<?php
+	$service_one_array = array(
+		'one' => __('Alternative Dispute Resolution', 'options_check')	);	
+			$service_one_sub_array = array(
+			'one' => __('How do Mediation and Arbitration Differ?', 'options_check')	);	
+	
+	$service_two_array = array(
+		'one' => __('Art, Entertainment and Sports', 'options_check')	);				
+		$service_two_sub_array = array(
+			'one' => __(' ', 'options_check')	);
+				
+	$service_three_array = array(
+		'one' => __('Bankruptcy Law', 'options_check')	);
+		$service_three_sub_array = array(
+			'one' => __('Credit Restoration', 'options_check'),
+			'two' => __('Wage Garnishment', 'options_check')	);
+			
+	$service_four_array = array(
+		'one' => __('Civil Litigation and Appeals', 'options_check')	);	
+		$service_four_sub_array = array(
+			'one' => __('Business Disputes', 'options_check'),
+			'two' => __('Construction Litigation', 'options_check'),
+			'three' => __('Contract Disputes', 'options_check'),
+			'four' => __('E-Discovery', 'options_check'),
+			'five' => __('Employment', 'options_check'),
+			'six' => __('Fiduciary Litigation', 'options_check'),
+			'seven' => __('Financial Elder Abuse', 'options_check'),
+			'eight' => __('Intellectual Property and Technology Disputes', 'options_check'),
+			'nine' => __('Personal Injury Litigation', 'options_check'),
+			'ten' => __('Tax, Trust and Estate Litigation', 'options_check'),
+			'eleven' => __('White Collar Civil Enforcement', 'options_check')	);
+	
+	$service_five_array = array(
+		'one' => __('Construction', 'options_check')	);		
+		$service_five_sub_array = array(
+			'one' => __(' ', 'options_check')	);
+
+	$service_six_array = array(
+		'one' => __('Corporate Law', 'options_check')	);		
+		$service_six_sub_array = array(
+			'one' => __('Breach of Fiduciary Duty', 'options_check'),
+			'two' => __('Business, Corporate and Partner Disputes/Dissolutions', 'options_check'),
+			'three' => __('Business Litigation', 'options_check'),
+			'four' => __('Business Torts', 'options_check'),
+			'five' => __('Commercial Collections', 'options_check'),
+			'six' => __('Contract Law', 'options_check'),
+			'seven' => __('Family Business', 'options_check'),
+			'eight' => __('Landlord Tenant Disputes', 'options_check'),
+			'nine' => __('Loan Modifications', 'options_check'),
+			'ten' => __('Shareholder Disputes', 'options_check'),
+			'eleven' => __('Start-Ups', 'options_check'),
+			'twelve' => __('Taxation', 'options_check')	);
+
+	$service_seven_array = array(
+		'one' => __('Criminal', 'options_check')	);
+		$service_seven_sub_array = array(
+			'one' => __(' ', 'options_check')	);
+
+
+	$service_eight_array = array(
+		'one' => __('Elder Law', 'options_check')	);	
+		$service_eight_sub_array = array(
+			'one' => __('Common Elder Law Issues', 'options_check'),
+			'two' => __('Estate Planning', 'options_check'),
+			'three' => __('Guardianship', 'options_check'),
+			'four' => __('Medicaid Planning', 'options_check'),
+			'five' => __('Probate Attorneys', 'options_check'),
+			'six' => __('Wills, Trusts, and Probate', 'options_check')	);		
+
+	$service_nine_array = array(
+		'one' => __('Labor and Employment', 'options_check')	);
+		$service_nine_sub_array = array(
+			'one' => __(' ', 'options_check')	);
+
+	
+	$service_ten_array = array(
+		'one' => __('Environmental', 'options_check')	);	
+		$service_ten_sub_array = array(
+			'one' => __(' ', 'options_check')	);
+
+	
+	$service_eleven_array = array(
+		'one' => __('Estate Planning, Asset Protection and Probate', 'options_check')	);	
+		$service_eleven_sub_array = array(
+			'one' => __('Asset Protection', 'options_check'),
+			'two' => __('Estate and Trust Administration', 'options_check')	);
+	
+	$service_twelve_array = array(
+		'one' => __('Family Law Services', 'options_check')	);	
+		$service_twelve_sub_array = array(
+			'one' => __('Adoptions', 'options_check'),
+			'two' => __('Child Custody', 'options_check'),
+			'three' => __('Child Support', 'options_check'),
+			'four' => __('Complex Property Division', 'options_check'),
+			'five' => __('Divorce', 'options_check'),
+			'six' => __('Paternity and Support', 'options_check'),
+			'seven' => __('Post-Judgment Modifications', 'options_check'),
+			'eight' => __('Premarital and Post-Marital Agreements', 'options_check'),
+			'nine' => __('Spousal Maintenance', 'options_check')	);	
+
+	$service_thirteen_array = array(
+		'one' => __('Immigration', 'options_check')	);
+		$service_thirteen_sub_array = array(
+			'one' => __('Permanent Visas', 'options_check'),
+			'two' => __('Temporary Visas', 'options_check'),
+			'three' => __('Deportation Removal for a Felony Criminal Conviction', 'options_check') 	);
+	
+	$service_fourteen_array = array(
+		'one' => __('Intellectual Property', 'options_check')	);	
+		$service_fourteen_sub_array = array(
+			'one' => __(' ', 'options_check')	);
+
+	$service_fifteen_array = array(
+		'one' => __('Personal Injury Law', 'options_check')	);	
+		$service_fifteen_sub_array = array(
+			'one' => __('Automobile Accident (Car Accidents)', 'options_check'),
+			'two' => __('Aviation Accident', 'options_check'),
+			'three' => __('Bicycle Accidents', 'options_check'),
+			'four' => __('Birth Injuries', 'options_check'),
+			'five' => __('Brain Injury', 'options_check'),
+			'six' => __('Burn Injuries', 'options_check'),
+			'seven' => __('Construction Accidents', 'options_check'),
+			'eight' => __('Defective Medical Product', 'options_check'),
+			'nine' => __('Dog Bite', 'options_check'),
+			'ten' => __('Drunk Driving Accidents', 'options_check'),
+			'eleven' => __('Elevator Accidents', 'options_check'),
+			'twelve' => __('Environmental Injury', 'options_check'),
+			'thirteen' => __('Farm Accidents', 'options_check'),
+			'fourteen' => __('Industrial Accidents', 'options_check'),
+			'fifteen' => __('Medical Malpractice', 'options_check'),
+			'sixteen' => __('Motorcycle Accidents', 'options_check'),
+			'seventeen' => __('Nursing Home Abuse', 'options_check'),
+			'eighteen' => __('Premises Liability', 'options_check'),
+			'nineteen' => __('Product Liability Claims', 'options_check'),
+			'twenty' => __('Slip and Fall Accidents', 'options_check'),
+			'twentyone' => __('Swimming Pool Accidents', 'options_check'),
+			'twentytwo' => __('Truck Accident/Commercial Vehicles', 'options_check'),
+			'twentythree' => __('Water Craft Accidents', 'options_check'),
+			'twentyfour' => __('Wrongful Death', 'options_check')	);	
+
+	$service_sixteen_array = array(
+		'one' => __('Real Estate Law', 'options_check')	);	
+		$service_sixteen_sub_array = array(
+			'one' => __('Closings', 'options_check'),
+			'two' => __('Easements', 'options_check'),
+			'three' => __('Financing and Mortgage Related Issues', 'options_check'),
+			'four' => __('Foreclosures', 'options_check'),
+			'five' => __('Land Use', 'options_check'),
+			'six' => __('Litigation', 'options_check'),
+			'seven' => __('Negotiating and Resolving Property Disputes', 'options_check'),
+			'eight' => __('Property Tax Appeals', 'options_check'),
+			'nine' => __('Tenant/Landlord Relationships', 'options_check'),
+			'ten' => __('Zoning Issues', 'options_check')	);	
+
+	$service_seventeen_array = array(
+		'one' => __('Tax Law', 'options_check')	);	
+		$service_seventeen_sub_array = array(
+			'one' => __('Audits', 'options_check'),
+			'two' => __('Business Tax', 'options_check'),
+			'three' => __('Estate and Gift Tax Planning', 'options_check'),
+			'four' => __('Executive Compensation', 'options_check'),
+			'five' => __('Tax Controversies', 'options_check'),
+			'six' => __('Tax Debt Relief', 'options_check'),
+			'seven' => __('Tax Exempt Organizations', 'options_check'),
+			'eight' => __('Tax Litigation', 'options_check'),
+			'nine' => __('Tax Planning', 'options_check')	);
+
+	$service_eighteen_array = array(
+		'one' => __('Farm Law', 'options_check')	);	
+
+?>
