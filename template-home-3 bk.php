27b0e2c6d14580621a7631dd0acd02f19a1eda29
Mike Harrigan <michaelharrigan@obelismedia.com>
2015-05-21 09:49:08 -0400

    remove boxes

---
+++ template-home-3 bk.php
@@ -0,0 +1,128 @@
+<?php
+/*
+ * Template Name: Home Page Version 3
+ * Description: Page without sidebar
+ */
+ ?>
+ <?php get_header(); ?>
+<?php /*
+<div id="myCarousel" class="carousel slide visible-lg visible-md">
+	<div class="carousel-inner">
+        <div class="item active" style="background-image: url(/wp-content/uploads/sites/12/2015/04/Queensboro-Bridge-over-New-Yor-web.jpg);background-position:center; background-size:cover;background-repeat: no-repeat;">
+        	<div class="container">
+            	<div class="carousel-caption">
+                	<h1>Personal Injury Justice for New York City</h1>
+                    <p>The Law Offices of Eric Richman specializes on Personal Injury law in New York City. Our team of experienced New York trial attorneys work with leading investigators, medial experts and financial experts to prepare a strong case. Our success is contingent on yours.</p>
+                </div><!-- /.carousel-caption -->
+            </div><!-- /.container -->
+        </div><!-- /.item -->
+	</div><!-- /.carousel-inner -->
+</div><!-- carousel -->
+*/ ?>
+<div class="fluid-container page-intro">
+    <img src="/wp-content/uploads/sites/12/2015/04/Queensboro-Bridge-over-New-Yor-web.jpg" alt="Queensboro Bridge leading to Upper East Side Manhattan New York City" />
+    <div class="container page-intro-info">
+    	<h1 class="page-headline fade-away" data-scroll-speed="2">Personal Injury Justice for New York City</h1>
+    </div>
+</div>
+
+<div class="container">
+	<div class="row">
+    	<div class="col-md-4 feature-box aspect-square">
+        	<div class="feature-box-content well vertical-stretch-xs vertical-justify">
+            <h2>In The News</h2>
+              <p>Eric Richman has been featured in countless news articles and stories.</p>
+              <a class="btn btn-primary horizontal-center" href="/media/">Get the Stories <span class="glyphicon glyphicon-arrow-right"></span></a>
+            </div>
+            <div class="feature-box-image">
+            	<img src="<?php bloginfo('template_url'); ?>/images/eric-richman-attorney-in-the-news.jpg" width="400" height="400" alt="Attorney In The News"/>
+            </div>
+        </div>
+        <div class="col-md-4 feature-box aspect-square">
+        	<div class="feature-box-content well vertical-stretch-xs vertical-justify">
+            <h2>TV Appearances</h2>
+              <p>See Eric Richman in action interviewed on CNN.</p>
+              <a class="btn btn-primary horizontal-center" href="#">See the Action <span class="glyphicon glyphicon-arrow-right"></span></a>
+            </div>
+            <div class="feature-box-image">
+            	<img src="<?php bloginfo('template_url'); ?>/images/eric-richman-attorney-tv-appearances.jpg" width="400" height="400" />
+            </div>
+        </div>
+        <div class="col-md-4 feature-box aspect-square">
+        	<div class="feature-box-content well vertical-stretch-xs vertical-justify">
+            <h2>Success Stories</h2>
+              <p>Eric Richman works for you producing real results.</p>
+              <a class="btn btn-primary horizontal-center" href="/results/">Read the Results <span class="glyphicon glyphicon-arrow-right"></span></a>
+            </div>
+            <div class="feature-box-image">
+            	<img src="<?php bloginfo('template_url'); ?>/images/eric-richman-attorney-success-stories.jpg" width="400" height="400" />
+            </div>
+        </div>
+    </div>
+    <div class="feature-divider"></div>
+	<div class="row height-fix">
+    	<div class="col-md-4">
+        	<div class="well info-box height-fix-item vertical-top horizontal-center">
+                <h2>Practice Information</h2>
+                <p>The Law Offices of Eric Richman provides outstanding personal injury legal representation to individuals and families who suffered injuries in and around New York, including New York City, Bronx, Queens, Manhattan, Brooklyn, Staten Island, Rockland, Westchester, Long Island, Nassau County or Suffolk County.</p>
+                <a class="btn btn-large btn-primary vertical-push-bottom" href="#">Read More <span class="glyphicon glyphicon-arrow-right"></span></a>
+            </div>
+        </div>
+        <div class="col-md-4">
+        	<div class="well info-box height-fix-item vertical-top horizontal-center">
+                <h2>About Eric Richman</h2>
+                <p>Eric Richman, Esq. is a passionate and experienced New York City personal injury lawyer who has dedicated his career to fighting for injury victims and their families.  Mr. Richman has won millions of dollars for his clients and is one of the best known and most respected personal injury lawyers in NYC </p>
+                <a class="btn btn-large btn-primary vertical-push-bottom" href="#">Read More <span class="glyphicon glyphicon-arrow-right"></span></a>
+            </div>
+        </div>
+        <div class="col-md-4">
+        	<div class="well info-box height-fix-item vertical-top horizontal-center">
+                <h2>Practice Areas</h2>
+                <ul>
+                    <li>Personal Injury</li>
+                    <li>Car and Truck Accidents</li>
+                    <li>Bus and Subway Accidents</li>
+                    <li>Motorcycle Bicycle Accidents</li>
+                    <li>Premises Liability</li>
+                    <li>Sexual Assualt Victims</li>
+                    <li>Medical Malpractice</li>
+                    <li>Wrongful Death and Catastrophic Injuries</li>
+                </ul>
+                <a class="btn btn-large btn-primary vertical-push-bottom" href="#">Read More <span class="glyphicon glyphicon-arrow-right"></span></a>
+            </div>
+        </div>
+	</div><!--/.row-->
+</div><!-- /.container -->
+<div class="container">
+<div class="feature-divider"></div>
+</div>
+<div class="container reviews">
+	<div class="jumbotron">
+        <div class="row">
+            <div class="col-md-4">
+                <div class="well">
+                <h3><span class="glyphicon glyphicon-star"></span><span class="glyphicon glyphicon-star"></span><span class="glyphicon glyphicon-star"></span><span class="glyphicon glyphicon-star"></span><span class="glyphicon glyphicon-star"></span><br /><small>Kelsey Plantas</small></h3><p>Eric is a great attorney. He is highly knowledgable, professional, and personable. He made me feel comfortable throughout the whole process of settling my case. Eric also made sure that I understood every step towards settlement, which made me feel more at ease with the decisions I had to make. I would definitely recommend working with Eric.</p>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="well">
+                <h3><span class="glyphicon glyphicon-star"></span><span class="glyphicon glyphicon-star"></span><span class="glyphicon glyphicon-star"></span><span class="glyphicon glyphicon-star"></span><span class="glyphicon glyphicon-star"></span><br /><small>Marisa Kronenberg</small></h3><p>Eric Richman is very honest, intelligent, and easy to talk to. He made himself available to me whenever I had questions and I always felt at ease knowing he was working hard for my case. He exceeded my expectations and I would recommend him to anyone looking for a personal injury lawyer!</p>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="well">
+                <h3><span class="glyphicon glyphicon-star"></span><span class="glyphicon glyphicon-star"></span><span class="glyphicon glyphicon-star"></span><span class="glyphicon glyphicon-star"></span><span class="glyphicon glyphicon-star"></span><br /><small>Paula Medeiros</small></h3><p>Extremely aggressive. Have already recommended him to a friend who also got in an accident. 10 out of 10!</p>
+                </div>
+                <div class="well">
+                <h3><span class="glyphicon glyphicon-star"></span><span class="glyphicon glyphicon-star"></span><span class="glyphicon glyphicon-star"></span><span class="glyphicon glyphicon-star"></span><span class="glyphicon glyphicon-star"></span><br /><small>Shad Azimi</small></h3><p>Excellent communicator with deep knowledge of the legal process. Also very responsive. Highly recommended.</p>
+                </div>
+            </div>
+        </div><!--/row-->
+        <div class="row">
+            <div class="col-xs-12">
+                <a class="btn btn-large btn-primary" href="https://plus.google.com/104401465280386732812/" target="_blank">Read More Reviews</a>
+            </div>
+    	</div><!-- /.row -->
+	</div><!-- /.jumbotron -->
+</div><!-- /.container -->
+<?php get_footer(); ?>
