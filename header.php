1a489c45cf9ddabda4dd2932552abff41a67d68f
Mike Harrigan <michaelharrigan@obelismedia.com>
2015-05-21 14:49:13 -0400

    header

--- header.php
+++ header.php
@@ -19,7 +19,7 @@
 	<div class="container header">
     	<div class="header-inner-container">
     	<div class="row vertical-center-sm">
-        	<div class="col-lg-4 col-md-6 col-sm-3">
+        	<div class="col-lg-4 col-md-5 col-sm-3">
 		<img src="<?php bloginfo('template_url'); ?>/images/eric-richman-law-temp-logo.svg" alt="The Law Offices of Eric Richman Attornery" class="img-responsive logo" />
         	</div>
             <div class="col-lg-8 col-md-6 col-sm-9">
