17c9cee67c626f524bb48e6be1a8d96d8fb3b33c
Mike <mikeh@ioprint.com>
2015-05-28 16:35:48 -0400

    footer edits

--- footer.php
+++ footer.php
@@ -9,9 +9,9 @@
           <div class="row">
 			<div class="col-md-12" id="footer-cta">
 				<hr />
-				<h2>REQUEST A FREE CONSULTATION</h2>				
+				<h2>Request A Free Consultation</h2>				
 				<h3>Fill out the form below to receive a free and confidential consultation.</h3>
-				 <h3 class="footer_header">Or Call Us At: <a href="tel:+12126883965"><span itemprop="telephone">212.688.3965</span></a></h3>
+				 <h3 class="footer_header">Or Call Us At: <a href="tel:+18008019655">800.801.9655</span></a> 24/7</h3>
 				<hr />
 				<?php echo do_shortcode('[gravityform id="1" name="Request A Free Consultation" title="false" description="false"]'); ?>
 
@@ -27,7 +27,7 @@
 
 
       <footer>
-      	&copy;<?php echo date("Y"); echo " "; bloginfo('name'); ?> | <a href="/privacy-policy/">Privacy Policy</a> <a href="/legal-disclaimer/">Legal Disclaimer</a>
+      	&copy;<?php echo date("Y"); echo " "; bloginfo('name'); ?> | <a href="/privacy-policy/">Privacy Policy</a> | <a href="/legal-disclaimer/">Legal Disclaimer</a>
       </footer>
 
 
